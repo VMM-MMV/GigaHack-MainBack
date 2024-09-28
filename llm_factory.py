16c5f29b578e899a9b95f87b110b5fe1654e836d
vmm-mmv <mihai.vieru@isa.utm.md>
2024-09-28 23:00:26 +0300

    improved llm factory

--- llm_factory.py
+++ llm_factory.py
@@ -2,12 +2,10 @@ from reader import get_sys_env
 from GroqLLM import GroqLLM
 from ServerLLM import ServerLLM
 
-def get_llm(llm_name=None):
-    llm = get_sys_env("LLM")
+def get_llm(llm=None):
+    if llm == None:
+        llm = get_sys_env("LLM")
     llm = llm.lower()
-    
-    if llm_name != None:
-        llm = llm_name.lower()
 
     match(llm):
         case "grok":
